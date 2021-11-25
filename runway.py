7187762591f2fbae34be37fe052075c536079e41
natekrth <natekrth@gmail.com>
2021-11-25 21:19:43 +0700

    add comment and docstring

--- runway.py
+++ runway.py
@@ -1,4 +1,4 @@
-from coordinate import Coordinate
+from test.coordinate import Coordinate
 from turtle import Turtle
 import copy
 
@@ -11,38 +11,43 @@ class Runway:
         self.length = length
         self.turtle = Turtle()
         self.turtle.pensize(2)
-        self.turtle.pencolor('white')
+        self.turtle.pencolor('white')       # runway color is white
         self.turtle.hideturtle()
-        self.turtle.shape('triangle')
-        self.turtle.shapesize(0.8,0.8,0.8)
+        self.turtle.shape('triangle')       # change turtle to be triangle symbol
+        self.turtle.shapesize(0.8,0.8,0.8)  # change turtle size
         self.turtle.speed('fastest')
-        self.__ils_pos_list = []
 
 
     @property
     def pos(self):
+        """getter method of position"""
         return self.__pos
 
     @property
     def heading(self):
+        """getter method of heading"""
         return self.__heading
 
     @property
     def width(self):
+        """getter method of width"""
         return self.__width
 
     @property
     def length(self):
+        """getter method of length"""
         return self.__length
 
     @pos.setter
     def pos(self, pos):
+        """setter method position and position must be Coordinate object"""
         if not isinstance(pos, Coordinate):
             raise TypeError('pos must be a Coordinate')
         self.__pos = copy.deepcopy(pos)
 
     @heading.setter
     def heading(self, heading):
+        """setter method heading and heading must be positive integer"""
         if not isinstance(heading, int):
             raise TypeError('heading must be integer')
         if heading < 0:
@@ -51,6 +56,7 @@ class Runway:
 
     @width.setter
     def width(self, width):
+        """setter method width and width must be positive integer"""
         if not isinstance(width, int):
             raise TypeError('width must be integer')
         if width < 0:
@@ -59,6 +65,7 @@ class Runway:
 
     @length.setter
     def length(self, length):
+        """setter method length and length must be positive integer"""
         if not isinstance(length, int):
             raise TypeError('length must be integer')
         if length < 0:
@@ -66,6 +73,10 @@ class Runway:
         self.__length = copy.deepcopy(length)
 
     def draw(self):
+        """
+        method draw will draw runway on the screen
+        by given position, heading, length, and width using turtle
+        """
         self.turtle.setheading(self.heading)
         for i in range(2):
             self.turtle.forward(self.length)
@@ -81,6 +92,11 @@ class Runway:
 
 
     def ils_data(self):
+        """
+        ils_data method return current ils x and y coordinate, and length of ils
+        max heading and min heading both are data use for checking heading for capture ils
+        check_top, check_bot, check_left, check_right are data use for checking position for capture ils
+        """
         ils_x = self.turtle.xcor()
         ils_y = self.turtle.ycor()
         ils_length = self.length / 1.5
